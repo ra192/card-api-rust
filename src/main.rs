8bf9964e2982b1cfd7b0eeeb10d52580ac9e36a5
ra192 <yakovlev.roman@gmail.com>
2021-09-14 19:29:01 +0300

    add deposit card

--- src/main.rs
+++ src/main.rs
@@ -44,30 +44,30 @@ async fn main() {
 
     let token_route = warp::path!("api"/"token").and(warp::post())
         .and(with_db(pool.clone())).and(warp::body::json())
-        .and_then(token::create_token_handler).with(log);
+        .and_then(token::create_token_handler);
 
     let fund_route = warp::path!("api"/"account"/"fund").and(warp::post())
         .and(with_db(pool.clone())).and(warp::header("Authorization"))
-        .and(warp::body::json()).and_then(transaction::fund_account_handler).with(log);
+        .and(warp::body::json()).and_then(transaction::fund_account_handler);
 
     let create_customer = warp::path!("api"/"customer").and(warp::post())
         .and(with_db(pool.clone())).and(warp::header("Authorization"))
-        .and(warp::body::json()).and_then(customer::create_handler).with(log);
+        .and(warp::body::json()).and_then(customer::create_handler);
 
     let create_card = warp::path!("api"/"card").and(warp::post())
         .and(with_db(pool.clone())).and(warp::header("Authorization"))
-        .and(warp::body::json()).and_then(card::create_virtual_handler).with(log);
+        .and(warp::body::json()).and_then(card::create_virtual_handler);
 
     let deposit_card = warp::path!("api"/"card"/"deposit").and(warp::post())
         .and(with_db(pool.clone())).and(warp::header("Authorization"))
-        .and(warp::body::json()).and_then(card::deposit_virtual_handler).with(log);
+        .and(warp::body::json()).and_then(card::deposit_virtual_handler);
 
     let withdraw_card = warp::path!("api"/"card"/"withdraw").and(warp::post())
         .and(with_db(pool.clone())).and(warp::header("Authorization"))
-        .and(warp::body::json()).and_then(card::withdraw_virtual_handler).with(log);
+        .and(warp::body::json()).and_then(card::withdraw_virtual_handler);
 
     let routes = token_route.or(fund_route).or(create_customer)
-        .or(create_card).or(deposit_card).or(withdraw_card);
+        .or(create_card).or(deposit_card).or(withdraw_card).with(log);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 8080))
